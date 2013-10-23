968eecc3ce57a9251e01bff81a47d2c315ba50e7
Ormaz <florian.duros@gmail.com>
2013-10-23 17:29:08 +0200

    Wait before launch client

--- src/katch.py
+++ src/katch.py
@@ -1,8 +1,9 @@
 import threading
+import time
 from connection import server, client
 
 t = threading.Thread(target=server.create_server)
-print("start")
 t.start()
-print("after")
+
+time.sleep(2)
 client.start_game()
