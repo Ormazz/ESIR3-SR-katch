f8b753f72409c2dd820cf2516288eb65fb000be9
Ormaz <florian.duros@gmail.com>
2013-11-04 17:32:33 +0100

    Gui and no gui

--- src/katch_no_gui.py
+++ src/katch_no_gui.py
@@ -1,6 +1,5 @@
 import threading
 import time
-from gui import game
 from connection import server,client,connectionManager
 import urllib.request
 
@@ -10,6 +9,5 @@ t = threading.Thread(target=server.create_server, args=(ip,))
 t.start()
 
 connectionManager.ConnectionManager()._ip_serv = ip
-#time.sleep(2)
-game.Game().main()
-#client.start_game()
+time.sleep(2)
+client.start_game()
