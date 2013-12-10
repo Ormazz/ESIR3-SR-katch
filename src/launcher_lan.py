63a7e45d824db6931f20a414f404f2401a7980cb
Clément <rivaille.clement@orange.fr>
2013-12-10 17:19:50 +0100

    Only the port needed for the launcher

--- src/launcher_lan.py
+++ src/launcher_lan.py
@@ -3,26 +3,31 @@ from gui import game
 from control import katch
 from connection import server,connectionManager
 import sys
+import socket
 
 # Launch of the game
 
 # Retrieving the local ip from the outside
-ip = sys.argv[1]
+if len(sys.argv) < 1:
+	print("Please give the port you wish to use for the server!")
+else:
+	port = sys.argv[1]
+	ip = socket.gethostbyname(socket.getfqdn()) + ":" + str(port)
 
-# Initializing the game and its component
-connectionManager.ConnectionManager()._ip_serv = ip
-myGame = game.Game()
-katch.Katch().init(connectionManager.ConnectionManager(), myGame.get_player_manager(), myGame.get_display_manager(), myGame.get_collectable_manager())
+	# Initializing the game and its component
+	connectionManager.ConnectionManager()._ip_serv = ip
+	myGame = game.Game()
+	katch.Katch().init(connectionManager.ConnectionManager(), myGame.get_player_manager(), myGame.get_display_manager(), myGame.get_collectable_manager())
 
-# Launching the server in another thread
-t = threading.Thread(target=server.create_server, args=(ip,))
-t.start()
+	# Launching the server in another thread
+	t = threading.Thread(target=server.create_server, args=(ip,))
+	t.start()
 
-# Launching the game
-myGame.main()
+	# Launching the game
+	myGame.main()
 
-# When the game has stopped, we have to send a message to other players
-katch.Katch().leave()
+	# When the game has stopped, we have to send a message to other players
+	katch.Katch().leave()
 
-# Shutting down the server
-t._stop()
+	# Shutting down the server
+	t._stop()
